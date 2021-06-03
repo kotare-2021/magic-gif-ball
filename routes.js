7c4958afd1a470cf1ecabe91d5372011f21e6746
lucas-f0ster <lucasf284@gmail.com>
2021-06-04 09:40:41 +1200

    router has get and post for test page, awaiting data from input field

--- routes.js
+++ routes.js
@@ -1,14 +1,23 @@
 const fs = require('fs')
 const express = require('express')
+// const data = require(__dirname+'/data.json')
+const data = {images:['img1', 'img2', 'img3']}
+const randomInt = require('./randomInt.js')
 
 const router = express.Router()
 
-router.get('', (req, res) => {
+router.get('/', (req, res) => {
   res.send('<h1>TEST<h1>')
 })
 
 router.get('/testPage', (req, res) => {
   res.sendFile(__dirname + '/testPage.html')
+})
+
+router.post('/testPage', (req, res) => {
+  var index = randomInt(data.length)
+  var image = data[index]
+  res.render('home', image)
 })
 
 module.exports = router
