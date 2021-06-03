3f88f1b78799b439e8e2874c36701fa1513f0db9
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-04 09:50:48 +1200

    write home.hbs template

--- server.test.js
+++ server.test.js
@@ -6,3 +6,17 @@ const server = require('./server')
 test('test suite is working', () => {
     expect(true).toBeTruthy()
 })
+
+test('/ renders correctly', (done) => {
+    expect.assertions(1)
+    // const expected = 'some txt'
+  
+    request(server)
+    .get('/')
+    .end((err, response) => {
+      expect(err).toBeNull()
+    //   const actual = response.text
+    //   expect(actual).toContain(expected)
+      done()
+    })
+  })
