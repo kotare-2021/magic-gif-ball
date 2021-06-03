641ec0630e14c930f714d71721280ab7703d7d8b
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-03 17:20:02 +1200

    install packages and set up file structure

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const fs = require('fs')
+const routes = require(__dirname + '/routes.js')
+
+const server = express()
+
+// Server configuration
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+// Your routes/router(s) should go here
