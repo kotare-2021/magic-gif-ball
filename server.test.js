f634376e90ec5b2346e1e5e35462240b8a4d9f73
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-04 08:36:03 +1200

    set up test suite with first test

---
+++ server.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest')
+const cheerio = require('cheerio')
+
+const server = require('./server')
+
+test('test suite is working', () => {
+    expect(true).toBeTruthy()
+})
